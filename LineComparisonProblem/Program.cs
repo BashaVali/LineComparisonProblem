977a2c225fb77bcacb515027b55ac12541a997a4
BashaVali <bashavali222@gmail.com>
2022-10-30 18:03:51 +0530

    Line Comparison Problem

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LineComparison
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparision Problem Statement");
+            CalculateLength Calculate = new CalculateLength(1, 2, 3, 4);
+            Calculate.Calculate();
+        }
+    }
+}
